d303a1da4185d2ee005d80517b20b229dd5185a5
Gabriel <gabrodriguez153@gmail.com>
2024-10-02 21:33:08 -0300

    Finalizando o Login e Iniciando o Cadastro

--- controllers/routes.py
+++ controllers/routes.py
@@ -5,3 +5,7 @@ def init_app(app):
     @app.route('/')
     def login():
         return render_template('login.html')
+    
+    @app.route('/signup')
+    def signup():
+        return render_template('signup.html')
