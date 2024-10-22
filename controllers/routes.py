8ca3df48d5cea8e7035628d7664cc078f8f370e3
Gabriel <gabrodriguez153@gmail.com>
2024-10-22 18:54:35 -0300

    fix: just bugs

--- controllers/routes.py
+++ controllers/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, request, url_for, redirect
 
 def init_app(app):
     
-    @app.route('/login')
+    @app.route('/')
     def login():
         return render_template('login.html')
     
