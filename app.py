b71d4696002e94393df4531ff8a5563f16603aeb
Gabriel <gabrodriguez153@gmail.com>
2024-10-29 20:52:16 -0300

    feat:completing the nav in the main

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from controllers import routes
 if __name__ == '__main__':
     app = Flask(__name__, template_folder='views')
     routes.init_app(app)
-    app.run(host='localhost', port=4100, debug=True)
+    app.run(host='localhost', port=4000, debug=True)
