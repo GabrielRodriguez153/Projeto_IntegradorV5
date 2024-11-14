f38c392fcbfbd7fa2c32e47c87dd8d79d888f022
Gabriel <gabrodriguez153@gmail.com>
2024-11-13 22:12:10 -0300

    fix: ajust the simplw verso

--- controllers/routes.py
+++ controllers/routes.py
@@ -26,16 +26,3 @@ def init_app(app):
     def perfil():
         return render_template('perfil.html')
     
-    @app.route('/data')
-    def data():
-        chart_data = {
-            "labels": ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"],
-            "datasets": [{
-                "label": "Casos de Antracnose",
-                "data": [65, 59, 80, 81, 56, 55],
-                "backgroundColor": "rgba(75, 192, 192, 0.2)",
-                "borderColor": "rgba(75, 192, 192, 1)",
-                "borderWidth": 1
-            }]
-        }
-        return jsonify(chart_data)
