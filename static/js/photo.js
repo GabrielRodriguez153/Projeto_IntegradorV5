4a73719f684870e7577e4ca271ecac0c80393716
Gabriel Henrique Rodrigues de Salles <99900712+GabrielRodriguez153@users.noreply.github.com>
2024-11-18 18:15:58 -0300

    Delete static directory

--- static/js/photo.js
+++
@@ -1,9 +0,0 @@
-function loadFile(event) {
-    const profilePic = document.querySelector(".mini-photo");
-    const file = event.target.files[0];
-
-    if (file) {
-        profilePic.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
-        profilePic.classList.add("has-image");  // Esconde o +
-    }
-}
