092e44f3307d0c40e2b2d2908fc6b1fd5dd35dc4
Gabriel <gabrodriguez153@gmail.com>
2024-11-21 22:24:58 -0300

    feat: ajust some things

--- Web/routes.py
+++ Web/routes.py
@@ -74,8 +74,8 @@ def init_app(app):
     
     @app.route('/main')
     def main():
-        user_id = session["user_id"]
-        return render_template('main.html', user_id=user_id)
+        user_name = session.get('user_name')
+        return render_template("main.html", user_name=user_name)
     
 
     @app.route('/history', methods=['GET'])
